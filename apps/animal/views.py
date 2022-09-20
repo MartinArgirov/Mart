dccc01f11a3b498231ec57de3ac62ec035b0fd86
Martin Argirov <mociiliev@hotmail.com>
2022-09-20 14:21:25 +0100

    atualização de fotos

--- apps/animal/views.py
+++ apps/animal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Apresentação de página estática
+def home(request):
+    return render(request, 'animal/home.html')
+
+def about(request):
+    return render(request, 'animal/about.html')
--- apps/pessoa/views.py
+++ apps/pessoa/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contacts(request):
+    return render(request, 'pessoa/contacts.html')
+
+def about(request):
+    return render(request, 'pessoa/about.html')
