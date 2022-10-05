c4376b2607f1ee31d5f47f1dfe4c5f5f8caab05a
Martin Argirov <mociiliev@hotmail.com>
2022-10-05 16:42:18 +0100

    despesa###

--- apps/despesa/models.py
+++ apps/despesa/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Despesa(models.Model):
-    valor = models.CharField(max_length=10)
+    despesa = models.CharField(max_length=30)
     categoria = models.CharField(max_length=30)
+    valor = models.CharField(max_length=10)
+    data = models.IntegerField()
+    mes = models.IntegerField()
     #idade = models.IntegerField(blank=True, null=True) # assim não tem nenhum valor
     #idade = models.IntegerField(default=0)# coloca idade automaticamente
-    mes = models.IntegerField()
+    
 
     def __str__(self):
         #return self.valor
